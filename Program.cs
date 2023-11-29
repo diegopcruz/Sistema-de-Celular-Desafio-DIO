4e221f41a5cb65163ae7728e85359f449a37cd4c
DIEGO CRUZ <136848287+diegopcruz@users.noreply.github.com>
2023-11-29 17:29:17 -0300

    Update Program.cs
    Exemplo.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Celular Iphone!");
+Iphone numeroIphone1 = new Iphone(numero: "94081-xxxx", modelo: "XII", imei: "123456", memoria: 256);
+numeroIphone1.ReceberLigacao();
+numeroIphone1.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Celular Nokia!");
+Nokia nokia = new Nokia(numero: "95468-xxxx", modelo: "NK75", imei: "123456", memoria: 256);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
+
+
